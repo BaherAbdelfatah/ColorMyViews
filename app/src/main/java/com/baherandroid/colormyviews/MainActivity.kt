d0bd8080543c5b926fe53709ed94cb627591a3dc
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-04-05 04:37:12 +0200

    Adding Our first Box

---
+++ app/src/main/java/com/baherandroid/colormyviews/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.baherandroid.colormyviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
